48fb59f93952b7b4a21ab1b1d8f854a1788b573e
vamigoya97 <amigo.victor.97@gmail.com>
2020-10-12 21:03:36 -0400

    lab completed

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Cell from './components/Cell'
 
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    var count = 0;
+    return vals.map(val => <Cell key={count++} value={val} />)
   }
   
   genMatrix = () => {
-    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    var count = 0;
+    return this.props.values.map(rowVals => <div className="row" key={count++}>{this.genRow(rowVals)}</div>)
   }
   
   render() {
@@ -18,4 +21,19 @@ export default class Matrix extends Component {
     )
   }
   
+}
+
+Matrix.defaultProps = {
+  values: [
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+  ]
 }
---
+++ src/components/Cell.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            color: props.value
+        }
+    }
+
+    changeColor = () => {
+        this.setState({
+            color: '#333'
+        })
+    }
+
+    render() {
+        return(
+            <div onClick={this.changeColor} className="cell" style={{backgroundColor: this.state.color}}>
+        
+            </div>
+        )
+    }
+}
